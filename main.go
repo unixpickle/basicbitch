db911d9592425459a6ccb712b9a100ba1024e592
Alex Nichol <unixpickle@gmail.com>
2016-03-03 20:44:55 +0000

    nicer output format
    Now, the output uses nice padding so all of the answers start on the same column. It also puts the signed and unsigned answers for each base on one row, which makes things a bit easier to read.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -20,9 +21,28 @@ func main() {
 		os.Exit(1)
 	}
 
+	maxLens := []int{0, 0}
 	for _, base := range []int{2, 8, 10, 16} {
-		fmt.Println("Signed answer in base", base, "is  ", answer.BaseStringSigned(base))
-		fmt.Println("Unsigned answer in base", base, "is", answer.BaseStringUnsigned(base))
+		ans := []string{answer.BaseStringSigned(base), answer.BaseStringUnsigned(base)}
+		for i, x := range ans {
+			if len(x) > maxLens[i] {
+				maxLens[i] = len(x)
+			}
+		}
 	}
-	fmt.Println("Overflow:", overflow)
+	fmt.Println("")
+	for _, base := range []int{2, 8, 10, 16} {
+		fmt.Println("Base", padStr(strconv.Itoa(base), 2), "|",
+			padStr(answer.BaseStringSigned(base), maxLens[0]),
+			"(signed) |", padStr(answer.BaseStringUnsigned(base), maxLens[1]),
+			"(unsigned)")
+	}
+	fmt.Println("\nOverflow:", overflow, "\n")
+}
+
+func padStr(s string, desiredLen int) string {
+	for len(s) < desiredLen {
+		s += " "
+	}
+	return s
 }
